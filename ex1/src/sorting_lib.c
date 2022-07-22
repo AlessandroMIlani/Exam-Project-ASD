457457c8ea8335350ca61d3c3f935362185b3b64
AlessandroMIlani <alessandromilani2000@gmail.com>
2022-07-22 16:39:05 +0200

    Start quicksort if insert sort > 30 sec

--- ex1/src/sorting_lib.c
+++ ex1/src/sorting_lib.c
@@ -1,10 +1,14 @@
 #include "sorting_lib.h"
 #include "generic_data.h"
 #include <stdlib.h>
+#include <time.h>
 
 #define RAND(min,max) ((rand()% (max - min +1))+min)
 
+
 void binary_insertion_sort(GenericArray* pointer, int (*compare)(void*, void*)) {
+	//start time
+	clock_t start = clock();
 	for (int i = 1;i < pointer->el_num;++i) {
 		void* val = (pointer->array)[i];
 		int placement = binary_search(pointer, val, 0, i - 1, compare);
@@ -14,6 +18,22 @@ void binary_insertion_sort(GenericArray* pointer, int (*compare)(void*, void*)) 
 			j--;
 		}
 		pointer->array[placement] = val;
+		//if time > 30 seconds, stop sorting
+		if ((clock() - start) / CLOCKS_PER_SEC > 30) {
+			printf("O nou...\n");
+			//whait 2 seconds
+			sleep(2);
+			printf("too much time...\n");
+			sleep(1);
+			printf("Distract the user!!!\n");
+			sleep(1);
+			system("sl");
+			sleep(1);
+			system("xdg-open https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+			//move to quick sort
+			i = pointer->el_num;
+			quick_sort(pointer, compare);
+		}
 	}
 }
 
@@ -30,34 +50,22 @@ int binary_search(GenericArray* pointer, void* val, int start, int end, int (*co
 	}
 }
 
-void quick_sort(GenericArray* pointer, int (*compare)(void*, void*),int (*pivot)(int l,int r)) {
-	quick_sort_rec(pointer, 0, pointer->el_num - 1, compare,pivot);
+void quick_sort(GenericArray* pointer, int (*compare)(void*, void*)) {
+	quick_sort_rec(pointer, 0, pointer->el_num - 1, compare);
 }
 
-void quick_sort_rec(GenericArray* pointer, int l, int r, int (*compare)(void*, void*),int (*pivot)(int l,int r)) {
+void quick_sort_rec(GenericArray* pointer, int l, int r, int (*compare)(void*, void*)) {
 	if (l < r) {
-		int p = pivot(l, r);
+		int p = rand_partition(l, r);
 		swap(pointer, l, p);
 		p = partition(pointer, &l, &r, compare);
-		quick_sort_rec(pointer, l, p, compare,pivot);
-		quick_sort_rec(pointer, p + 1, r, compare,pivot);
+		quick_sort_rec(pointer, l, p, compare);
+		quick_sort_rec(pointer, p + 1, r, compare);
 	}
 }
 
 int rand_partition(int l, int r) {
 	return RAND(l, r);
-}
-
-int leftmost_partition(int l,int r){
-	return l;
-}
-
-int rightmost_partition(int l,int r){
-	return r;
-}
-
-int centerof_partition(int l,int r){
-	return (l+r)/2;
 }
 
 int partition(GenericArray* pointer, int* l, int* r, int (*compare)(void*, void*)) {
